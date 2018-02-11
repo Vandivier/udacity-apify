a04441069dd89312bae7b0f5e9be653c22088581
Vandivier <vandivier_john@yahoo.com>
2018-02-11 16:07:15 -0500

    sLocation and more;
    better last updated date;
    better page closing;
    slightly longer page wait;

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ const oTitleLine = {
     'sId': 'Entry ID',
     'sEmail': 'Email Address',
     'sUserName': 'Username',
+    'sLocation': 'Location',
     'iEducationCount': 'Count of Education Entries',
     'sLinkedInUrl': 'LinkedIn Url',
     'sResumeUrl': 'Resume Url',
@@ -153,9 +154,9 @@ async function fpScrapeInputRecord(oRecord) {
             // toast message will disappear if you wait too long
             await _fpWait(1000);
 
-            let arr$Affiliations = $('#affiliation-body a[name=subaffil]');
-            let sarrAffiliations = '';
-            let _oResult = {
+            var arr$Affiliations = $('#affiliation-body a[name=subaffil]');
+            var sarrAffiliations = '';
+            var _oResult = {
                 'bUserExists': $('[class*=profile-container]').length > 0,
                 'bProfileIsPrivate': $('[class*="toast--message"]').html() === 'Profile is private',
                 'bTooManyRequestsError': _fsSafeTrim($('[class*="toast--message"]').html()) === 'Too many requests'
@@ -165,16 +166,17 @@ async function fpScrapeInputRecord(oRecord) {
             if (!_oResult.bProfileIsPrivate
                 && !_oResult.bTooManyRequestsError)
             {
-                await _fpWait(3000);
+                await _fpWait(4000);
 
                 _oResult.sUserName = $('h1[class*="user--name"]').html();
+                _oResult.sLocation = $('[alt="location marker"]').next().html();
                 _oResult.sEmail = $('.emaillabel').parent().find('td span').text();
                 _oResult.iEducationCount = $('div[class*="educations--section"] div[class*="_education--education"]').length;
                 _oResult.sLinkedInUrl = $('a[title="LINKEDIN"]').attr('href');
                 _oResult.sResumeUrl = $('a[title="Resume"]').attr('href');
                 _oResult.bOtherError = false;
                 _oResult.bPresentlyEmployed = $('div[class*="works--section"] div[class*="_work--work"] span[class*="_work--present"]').length > 0;
-                _oResult.sProfileLastUpdate = $('div[class*="profile--updated"]').text().split(': ')[1];
+                _oResult.sProfileLastUpdate = $('div[class*="profile--updated"]').text();
 
                 arr$Affiliations && arr$Affiliations.each(function (arr, el) {
                     let sTrimmed = _fsSafeTrim(el.innerText.replace(/\s/g, ' '));
@@ -211,10 +213,9 @@ async function fpScrapeInputRecord(oRecord) {
                 'bOtherError': true
             };
         });
-
-        await _page.close();
     }
 
+    await _page.close();
     oMergedRecord = Object.assign(oRecord, oScrapeResult);
     oCache.people[oRecord.sId] = JSON.parse(JSON.stringify(oMergedRecord));
     return Promise.resolve(JSON.parse(JSON.stringify(oRecord))); // return prior to merging to minimize invalid data passed on
