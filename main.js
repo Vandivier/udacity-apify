a6bbbff05a009d6c4c747318c9977cbf7e30ae4d
Vandivier <vandivier_john@yahoo.com>
2018-02-17 11:46:12 -0500

    input has priority over process.ENV
    so u don't have to rebuild docker with new env everytime
    bump ver no

--- main.js
+++ main.js
@@ -66,8 +66,8 @@ async function main() {
     const oInput = await Apify.getValue('INPUT');
     let arrsFirstNames;
 
-    sProxyPassword = process.env.APIFY_PROXY_PASSWORD || oInput.APIFY_PROXY_PASSWORD;
-    sProxyUser = process.env.APIFY_PROXY_USER || oInput.APIFY_PROXY_USER;
+    sProxyPassword = oInput.APIFY_PROXY_PASSWORD || process.env.APIFY_PROXY_PASSWORD;
+    sProxyUser = oInput.APIFY_PROXY_USER || process.env.APIFY_PROXY_USER;
 
     //await utils.fpWait(5000); // for debugging
 
